8d2b384ebdb5df2d8f39c3246c0403ea11e9c204
Soumya Ranjan Nayak <soumyarj904@gmail.com>
2025-04-15 21:31:57 +0530

    feat: Initialize React application with essential files and components
    - Added manifest.json for PWA support.
    - Created robots.txt for web crawlers.
    - Implemented basic App.test.js for initial testing.
    - Added logo assets (JPEG and SVG formats).
    - Established common CSS styles in common.css.
    - Developed Header component with styling.
    - Created TabContainer component for tab management.
    - Set up index.js and index.css for application entry point and global styles.
    - Implemented Controller component for routing and layout.
    - Created Home component as the landing page.
    - Added utility fetch.js for API calls.
    - Set up testing environment with setupTests.js.

---
+++ src/screens/Controller.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Home from "../screens/home/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "../common/header/Header";
+
+const Controller = () => {
+  const baseUrl = "/api/v1/";
+  return (
+    <Router>
+      <div className="main-container">
+        <Header />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Home {...props} baseUrl={baseUrl} />}
+        />
+      </div>
+    </Router>
+  );
+};
+
+export default Controller;
